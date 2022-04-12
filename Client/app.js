7c25936d3cce0417401b3a9679633877cb249e7d
piotrfijol <piotr.fijol1@gmail.com>
2022-04-12 16:17:07 +0200

    Added handling of 'message' event

--- Client/app.js
+++ Client/app.js
@@ -1,5 +1,34 @@
 import {io} from 'socket.io-client';
 
-io.connect("http://localhost:3000");
+const socket = io.connect("http://localhost:3000");
 
-console.log("Połączono");
+const sendMessage = document.querySelector("#send-message");
+const messageContent = document.querySelector("input#message")
+sendMessage.addEventListener('click', e => {
+    socket.emit("message", {
+        message: messageContent.value,
+    }); 
+})
+
+socket.on("message", (data) => {
+    newMessage(data.message);
+});
+
+const messageBox = document.querySelector(".chat__message-box");
+
+function newMessage(msg) {
+    const newMsg = document.createElement("div");
+    newMsg.classList = "chat__message-box__message";
+    
+    const msgUsername = document.createElement("p");
+    msgUsername.classList = "chat__message-box__message__username";
+    msgUsername.textContent = "username";
+
+    const msgContent = document.createElement("p");
+    msgContent.classList = "chat__message-box__message__content";
+    msgContent.textContent = msg;
+    
+    newMsg.appendChild(msgUsername);
+    newMsg.appendChild(msgContent);
+    messageBox.appendChild(newMsg);
+}
